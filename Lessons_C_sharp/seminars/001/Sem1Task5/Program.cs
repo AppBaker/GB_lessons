fd3f3b48e8e6c3094e82c9d21690d8ef6c4cf130
Ivan Nikitin <qqaqq@ya.ru>
2022-09-02 22:35:25 +0700

    Add Sem1Task5

---
+++ Lessons_C_sharp/seminars/001/Sem1Task5/Program.cs
@@ -0,0 +1,21 @@
+﻿//=============================================
+// #5 Напишите программу которая на вход принимает одно число (N)
+// а на выходе показывает все целые числа от -N до N
+//=============================================
+
+Console.WriteLine("Enter number N");
+string? inputLineN = Console.ReadLine();
+
+if (inputLineN != null)
+{
+    int numberN = int.Parse(inputLineN);
+    
+    int startNumberN = (-1)*numberN;
+
+    while (startNumberN<=numberN)
+    {
+        Console.Write(startNumberN + ", ");
+        startNumberN += 1;
+    }
+
+}
