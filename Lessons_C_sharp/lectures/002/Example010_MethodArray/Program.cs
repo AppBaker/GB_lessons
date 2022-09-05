19f36091ace66ed06d21129372669f3dcb6e4fb2
Ivan Nikitin <qqaqq@ya.ru>
2022-09-05 17:22:52 +0700

    Leccond C# lection 2 finish

---
+++ Lessons_C_sharp/lectures/002/Example010_MethodArray/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int[] array = { 1, 2, 3, 44, 5, 6, 7, 34 };
+
+int n = array.Length;
+
+int find = 44;
+int index = 0;
+
+while (index < n)
+{
+    
+    if (array[index] == find)
+    {
+        Console.WriteLine("find: " + array[index] + " at index = " + index);
+        break;
+    }
+    
+    index ++;
+Console.WriteLine(index);
+}
---
+++ Lessons_C_sharp/lectures/002/Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,63 @@
+﻿
+int[] array = new int[10];
+
+void FillArray(int[] collection)
+{
+    int lenth = collection.Length;
+    int index = 0;
+    while (index < lenth)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(array[position] + ", ");
+        position ++;
+    }
+}
+
+void IndexOf(int[] array, int find)
+{
+    int count = array.Length;
+    int index = 0;
+    while (index < count)
+    {
+        if (array[index] == find)
+        {   
+            Console.WriteLine("");
+            Console.WriteLine("find: " + array[index] + " at index = " + index);
+            break;
+        }
+        index ++;
+    }
+}
+
+
+FillArray(array);
+PrintArray(array);
+IndexOf(array, 5);
+
+// int n = array.Length;
+
+// int find = 44;
+// int index = 0;
+
+// while (index < n)
+// {
+    
+//     if (array[index] == find)
+//     {
+//         Console.WriteLine("find: " + array[index] + " at index = " + index);
+//         break;
+//     }
+    
+//     index ++;
+// Console.WriteLine(index);
+// }
