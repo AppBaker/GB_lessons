c7c2fcce39b66436bfd2f1b87494538cbb5c1b1c
Ivan Nikitin <qqaqq@ya.ru>
2022-09-05 15:20:50 +0700

    Lecture 002

---
+++ Lessons_C_sharp/lectures/002/Example008_introMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿
+// Задаем функцию
+double f(double x)
+{
+    double result = x * x + + 1;
+    return result;
+}
+
+
+Console.WriteLine(f(10));
+
+int x = 10;
+
+void ggg()
+{
+    x = x + 1;
+}
+
+ggg();
+Console.WriteLine(x);
+ggg();
+Console.WriteLine(x);
---
+++ Lessons_C_sharp/lectures/002/Example009_introArray/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+
+//              0  1  2   3  4  5  6  7  8  9
+int[] array = {13,22,433,14,65,96,57,88,99,100};
+
+Console.WriteLine(array.Contains(6));
+
+int[] arrayOne = new int[]{0,0,0,0,0};
+
+
+Console.WriteLine(array[4]);
