2f49d8b83c6eb78483eabb756f3eaaafd81b4f87
Ivan Nikitin <qqaqq@ya.ru>
2022-09-06 16:03:21 +0700

    Add Sem2Task10 home work

--- Lessons_C_sharp/seminars/002/README.md
+++ Lessons_C_sharp/seminars/002/README.md
@@ -1,7 +1,8 @@
 ## Задачи второго семинара
 
 1. [Sem2Task9](/Lessons_C_sharp/seminars/002/Sem2Task9/Program.cs)
-2. [Sem2Task11](/Lessons_C_sharp/seminars/002/Sem2Task11/Program.cs)
+2. [Sem2Task10](/Lessons_C_sharp/seminars/002/Sem2Task1/Program.cs) -> Home work
+3. [Sem2Task11](/Lessons_C_sharp/seminars/002/Sem2Task11/Program.cs)
 3. [Sem2Task12](/Lessons_C_sharp/seminars/002/Sem2Task12/Program.cs)
 4. [Sem2Task14](/Lessons_C_sharp/seminars/002/Sem2Task14/Program.cs)
 
---
+++ Lessons_C_sharp/seminars/002/Sem2Task10/Program.cs
@@ -0,0 +1,85 @@
+﻿//=============================================
+// #10 Напишите программу, которая принимает на вход трёхзначное число и на выходе
+//  показывает вторую цифру этого числа.
+//=============================================
+// Учимся структурированному программированию
+
+
+// Вариант 1
+
+// Глобальные переменные
+string inputLine = "";
+int result = 0;
+
+// Вводим данные
+void ReadData()
+{
+    Console.WriteLine("Введите трехзначное число");
+    inputLine = Console.ReadLine();
+}
+
+// Преобразование данных и выделение второй цифры
+void CalculateData()
+{
+    if (inputLine != null)
+    {
+        int number = int.Parse(inputLine);
+        result = (number / 10) % 10;
+    }
+    else
+    {
+        // Если ввели не трехзначное число, просим ввести другое число
+        Console.WriteLine("You entered an invalid number, please try again.");
+        ReadData();
+        CalculateData();
+    }
+}
+
+// Выводит результат
+void PrintData()
+{
+    Console.WriteLine("Вторая цифра: " + result);
+}
+
+
+
+// // Вариант 2
+// // Глобальные переменные
+// string inputLine = "";
+// string result = "";
+
+// // Вводим данные
+// void ReadData()
+// {
+//     Console.WriteLine("Введите трехзначное число");
+//     inputLine = Console.ReadLine();
+// }
+
+// // Преобразование данных и выделение второй цифры
+// void CalculateData()
+// {
+//     if (inputLine != null && inputLine.Length == 3)
+//     {
+//         char[] charArray = inputLine.ToCharArray();
+//         result = charArray[1].ToString();
+//     }
+//     else
+//     {
+//         // Если ввели не трехзначное число, просим ввести другое число
+//         Console.WriteLine("You entered an invalid number, please try again.");
+//         ReadData();
+//         CalculateData();
+//     }
+// }
+// // Выводит результат
+// void PrintData()
+// {
+//     Console.WriteLine("Вторая цифра: " + result);
+// }
+
+// =====================================================
+
+// Вызов функций
+ReadData();
+CalculateData();
+PrintData();
