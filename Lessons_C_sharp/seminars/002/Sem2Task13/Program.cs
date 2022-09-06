13c8917d297e0b2fba0c0ed892f45d2440e43c45
Ivan Nikitin <qqaqq@ya.ru>
2022-09-06 16:22:13 +0700

    Add Sem2Task13 home work

---
+++ Lessons_C_sharp/seminars/002/Sem2Task13/Program.cs
@@ -0,0 +1,42 @@
+﻿//=============================================
+// #13 Напишите программу, которая выводит третью цифру заданного
+//  числа или сообщает, что третьей цифры нет.
+//=============================================
+// Учимся структурированному программированию
+
+// Глобальные переменные
+string inputLine = "";
+string result = "";
+
+// Вводим данные
+void ReadData()
+{
+    Console.WriteLine("Введите трехзначное число");
+    inputLine = Console.ReadLine();
+}
+
+// Преобразование данных и выделение второй цифры
+void CalculateData()
+{
+    if (inputLine.Length > 2)
+    {
+        char[] charArray = inputLine.ToCharArray();
+        result = charArray[2].ToString();
+    }
+    else if (inputLine.Length < 3)
+    {
+        result = "третьей цифры нет";
+    }
+}
+// Выводит результат
+void PrintData()
+{
+    Console.WriteLine(inputLine + " -> " + result);
+}
+
+// =====================================================
+
+// Вызов функций
+ReadData();
+CalculateData();
+PrintData();
