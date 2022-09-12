f339ba6506e44674a3fd5d715b63112c9255908b
Ivan Nikitin <qqaqq@ya.ru>
2022-09-12 10:21:22 +0700

    Ass Sem4TaskRandomName home work

---
+++ Lessons_C_sharp/seminars/004/Sem4TaskRandomName/Program.cs
@@ -0,0 +1,46 @@
+﻿//=============================================
+// Написать программу которая из имен через запятую выберет случайное
+// имя и выведет в терминал
+// Игорь, Антон, Сергей -> Антон
+//=============================================
+
+//Запрос данных у пользователя
+// Возвращаем кортеж для проверки наличия имени
+(string, bool) ReadData(string line)
+{
+    //Выводим сообщение
+    Console.WriteLine(line);
+    string result = Console.ReadLine() ?? "No Names, fatal error :)";
+    if (result == "") return ("Вы не ввели ни одного имени!", false);
+    return (result, true);
+}
+
+// Разделяем имена и возвращаем случайное
+string GetRandomName(string names)
+{
+    string[] namesArray = names.Split(",");
+    Random random = new Random();
+    return namesArray[random.Next(0, namesArray.Length)];
+}
+
+// Вывод результата
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+ 
+// Выполнение программы
+// Собираем кортеж чтобы проверять ввел ли пользователь хотябы одно имя
+(string, bool) names = ReadData("Введите имена игроков через запятую (Ваня, Слава, Катя, Тима, Коля...)");
+
+if (names.Item2)
+{
+    PrintResult($"Отлично! Выйграл {GetRandomName(names.Item1)}");
+}
+else
+{
+    PrintResult($"{names.Item1}");
+}
+// Это выражение не работает! Почему?
+// (bool)names.Item2 ? PrintResult($"Отлично! Выйграл {GetRandomName(names.Item1)}") : PrintResult($"{names.Item1}");
+
