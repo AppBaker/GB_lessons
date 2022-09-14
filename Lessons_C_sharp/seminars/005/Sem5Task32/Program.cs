a9763dcc3dd055791be8dcdce4a1d3157df3ea37
Ivan Nikitin <qqaqq@ya.ru>
2022-09-14 23:59:19 +0700

    Add Sem5Task32

---
+++ Lessons_C_sharp/seminars/005/Sem5Task32/Program.cs
@@ -0,0 +1,74 @@
+﻿//=============================================
+// #32 Напишите программу замена элементов массива:
+// положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// Пример:
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+//=============================================
+
+//Запрос данных у пользователя
+int ReadData(string line)
+{
+    //Выводим сообщение
+    Console.WriteLine(line);
+    int result = int.Parse(Console.ReadLine() ?? "0");
+    return result;
+}
+
+// Метод, принимает строку, выводит в консоль
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+//Универсальный метод генерации и заполнения массива
+int[] FillArray(int num, int downBorder, int topBorder)
+{
+    //Генератор случайных чисел
+    Random numSintezator = new Random();
+    //Создаем массив
+    int[] arr = new int[num];
+    //Тест границ
+    if (downBorder > topBorder)
+    {
+        int temp = downBorder;
+        downBorder = topBorder;
+        topBorder = temp;
+    }
+    //Заполняем массив
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = numSintezator.Next(downBorder, topBorder + 1);
+    }
+    //Возвращаем результат
+    return arr;
+}
+
+//Печатает одномерный массив
+void Print1DArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1]);
+}
+
+int[] InversArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * (-1);
+    }
+    return array;
+}
+
+int arrayLenth = ReadData("Enter Array lenth");
+int downBorder = ReadData("Введите нижнюю границу заполнения массива: ");
+int topBorder = ReadData("Введите верхнюю границу заполнения массива: ");
+
+int[] inputArray = FillArray(arrayLenth, downBorder, topBorder);
+Print1DArr(inputArray);
+Print1DArr(InversArray(inputArray));
+
+
