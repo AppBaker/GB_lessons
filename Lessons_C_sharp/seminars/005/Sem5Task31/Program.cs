90555b05b0215ce928557827e951734dfe11af07
Ivan Nikitin <qqaqq@ya.ru>
2022-09-14 22:52:32 +0700

    Add Sem5Task31

---
+++ Lessons_C_sharp/seminars/005/Sem5Task31/Program.cs
@@ -0,0 +1,82 @@
+﻿//=============================================
+// #31 Задайте массив из 12 элементов, заполненный
+// случайными числами из промежутка [-9, 9].
+// Найдите сумму отрицательных и положительных
+// элементов массива.
+ 
+//=============================================
+
+//Запрос данных у пользователя
+int ReadData(string line)
+{
+    //Выводим сообщение
+    Console.WriteLine(line);
+    int result = int.Parse(Console.ReadLine() ?? "0");
+    return result;
+}
+
+//Универсальный метод генерации и заполнения массива
+int[] FillArray(int num, int downBorder, int topBorder)
+{
+    //Генератор случайных чисел
+    Random numSintezator = new Random();
+    //Создаем массив
+    int[] arr = new int[num];
+    //Тест границ
+    if (downBorder < topBorder)
+    {
+        //Заполняем массив
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = numSintezator.Next(downBorder, topBorder+1);
+        }
+    }
+    //Возвращаем результат
+    return arr;
+}
+
+//Печатае одномерный массив
+void Print1DArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1]);
+}
+
+int[] NegotivPositivSums(int[] arr)
+{
+    int[] sums = new int[2];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            sums[0] += arr[i];
+        }
+        else
+        {
+            sums[1] += arr[i];
+        }
+    }
+    return sums;
+}
+
+// Метод, принимает строку, выводит в консоль
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+int arrayLength = ReadData("Введите длину массива: ");
+int downBorder = ReadData("Введите нижнюю границу заполнения массива: ");
+int topBorder = ReadData("Введите верхнюю границу заполнения массива: ");
+
+int[] inputArray = FillArray(arrayLength,downBorder,topBorder);
+Print1DArr(inputArray);
+
+int[] sumArray = NegotivPositivSums(inputArray);
+
+PrintResult("Сумма >0: "+sumArray[0]+ " Сумма <0: "+sumArray[1]);
+Print1DArr(sumArray);
+
