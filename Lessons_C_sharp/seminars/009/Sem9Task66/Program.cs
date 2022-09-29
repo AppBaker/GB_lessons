c42644255d26131020a025ca8c2ff02dba86d243
Ivan Nikitin <qqaqq@ya.ru>
2022-09-29 20:24:22 +0700

    Sem9Task66 Home work

---
+++ Lessons_C_sharp/seminars/009/Sem9Task66/Program.cs
@@ -0,0 +1,34 @@
+﻿//=============================================
+// #66  Задайте значения M и N. Напишите программу,
+//  которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+//=============================================
+
+//Запрос данных у пользователя
+int ReadData(string line)
+{
+    //Выводим сообщение
+    Console.WriteLine(line);
+    int result = int.Parse(Console.ReadLine() ?? "0");
+    return result;
+}
+
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+int SumSubsequence(int start, int stop, int sum)
+{
+    //Точка остановки
+    if (start == stop) return sum + start;
+    sum += start + SumSubsequence(start + 1, stop, sum);
+    return sum;
+}
+
+// Исполнение программы
+int numM = ReadData("Введите число M: ");
+int numN = ReadData("Введите число N: ");
+PrintResult($"M = {numM}; N = {numN} -> {SumSubsequence(numM, numN, 0)}");
